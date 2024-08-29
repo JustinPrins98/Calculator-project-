33ea962cc979aa5c785cc16f1e0b14b2978a58a9
Justin Prins <justinprinsroc1998@gmail.com>
2024-08-29 11:30:12 +0200

    Made function substract, multiply and divide.

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 
 function add(a, b) {
     return a + b;
-}
+};
+console.log(add(5, 2));
 
-console.log(add(2, 3))
+
+function substract(a, b) {
+    return a - b;
+};
+console.log(substract(5, 2));
+
+function multiply(a, b) {
+    return a * b;
+};
+console.log(multiply(5, 2));
+
+function divide(a, b) {
+    return a / b;
+};
+console.log(divide(5, 2));
+
+
+
+
+
