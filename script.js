bdf5bffca375e6541de7d953d92fa7f70d22dc54
Justin Prins <justinprinsroc1998@gmail.com>
2024-08-29 12:31:35 +0200

    Working on a operate function still having to test is.

--- script.js
+++ script.js
@@ -1,24 +1,45 @@
+let firstNumber;
+let lastNumber;
+let operator;
+
+
+
+
+
 
 function add(a, b) {
     return a + b;
 };
-console.log(add(5, 2));
-
 
 function substract(a, b) {
     return a - b;
 };
-console.log(substract(5, 2));
 
 function multiply(a, b) {
     return a * b;
 };
-console.log(multiply(5, 2));
 
 function divide(a, b) {
     return a / b;
 };
-console.log(divide(5, 2));
+
+// Takes an operator and 2 numbers and then calls one of the above functions on the numbers.
+function operate(a, o, b) {
+    firstNumber == a;
+    lastNumber == b;
+
+    if (operator == "+") {
+        return add(firstNumber, lastNumber);
+    } else if (operator == "-") {
+        return substract(firstNumber, lastNumber);
+    } else if (operator == "*") {
+        return multiply(firstNumber, lastNumber);
+    } else if (operator == "/") {
+        return divide(firstNumber, lastNumber);
+    } else { "invalid syntax" };
+    
+};
+
 
 
 
